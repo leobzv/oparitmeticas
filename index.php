6b8bcbd60a50973fd94c5ecc242cf727a5a6eda6
Leonardo Bermudez <ing.leobermudez@gmail.com>
2017-04-01 21:58:19 -0300

    operaciones aritmeticas

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <?php
- echo "Que divino momento sensaciones vienen y van esto es una adiccion jujujaja";
+ $num1=75;
+ $num2=80;
+
 ?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<TABLE BORDER>
+<tr>
+    <td colspan="5" align="center"> EJEMPLO </td> 
+  </tr>
+	<TR>
+		<TH></TH> <TH>NUM1</TH> <TH>NUM2</TH><TH>RESULTADO</TH>
+	</TR>
+	<TR>
+		<TD>SUMA</TD> <TD><?php echo $num1; ?></TD> <TD><?php echo $num2; ?></TD><TD><?php echo $num1+$num2; ?></TD>
+	</TR>
+	<TR>
+		<TD>RESTA</TD> <TD><?php echo $num1; ?></TD> <TD><?php echo $num2; ?></TD><TD><?php echo $num1-$num2; ?></TD>
+	</TR>
+	<TR>
+		<TD>MULTIPLICAR</TD> <TD><?php echo $num1; ?></TD> <TD><?php echo $num2; ?></TD><TD><?php echo $num1*$num2; ?></TD>
+	</TR>
+	<TR>
+		<TD>DIVISION</TD> <TD><?php echo $num1; ?></TD> <TD><?php echo $num2; ?></TD><TD><?php echo $num1/$num2; ?></TD>
+
+	</TR>
+</TABLE>	
+
+
+
+</body>
+</html>
